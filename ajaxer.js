0b7fe84b460cbe59dd9b6d18f241545e52d290d9
Eddie Gurnee <pegurnee@gmail.com>
2015-09-14 15:43:26 -0400

    fixed most of this mess

--- ajaxer.js
+++ ajaxer.js
@@ -1,36 +1,53 @@
-var AjaxHandler = (function () {
+var AjaxHandler = (function() {
 
-  function AjaxHandler() {
+  function AjaxHandler(url = "", callBackFunction = "") {
     this.request = new XMLHttpRequest();
-    this.outgoingData = {};
+    this.destination = url;
+    this.callBackFunction = callBackFunction;
   }
 
-  AjaxHandler.prototype.setURL(theUrl) {
-    this.destination = theUrl;
-  }
-
-  AjaxHandler.prototype.addParam(newParam) {
-
-  }
-
-  AjaxHandler.prototype.setCallback(fn) {
-    this.request.onreadystatechange = fn;
-  }
-
-  AjaxHandler.prototype.post() {
-    connect("POST", this);
-  }
-
-  AjaxHandler.prototype.get() {
-    connect("GET", this);
-  }
-
-  function connect(style, ajaxHandler) {
-    if (style === "GET") {
-      
+  AjaxHandler.prototype.url = function(theUrl) {
+    if (theUrl === undefined) {
+      return this.destination;
+    } else {
+      this.destination = theUrl;
     }
-    ajaxHandler.request.open(style, ajaxHandler.destination, true);
+  }
 
+  AjaxHandler.prototype.callback = function(fn) {
+    this.request.onreadystatechange = function() {
+      if (this.request.readyState === 4 && this.request.status === 200) {
+        fn(this.request.responseText);
+      }
+    }
+  }
+
+  AjaxHandler.prototype.post = function(data) {
+    this.connect("POST", data, this.destination);
+  }
+
+  AjaxHandler.prototype.get = function(data) {
+    this.connect("GET", data, this.destination);
+  }
+
+  AjaxHandler.prototype.connect = function(style, data, destination) {
+    var sendData = Object.keys(data).map(function(k) {
+        return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+      }).join("&");
+    if (destination === undefined) {
+      destination = this.destination;
+    }
+    if (style === "GET") {
+      var urlParams = "?" + sendData;
+      destination += urlParams;
+      sendData = "";
+    } else {
+      this.request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      this.request.setRequestHeader("Content-length", sendData.length);
+      this.request.setRequestHeader("Connection", "close");
+    }
+    this.request.open(style, destination, true);
+    this.send(sendData);
   }
 
   return AjaxHandler;
