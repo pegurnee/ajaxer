f37d64e1ea7568bbf1aa43e68c305ddb27c2edc3
Eddie Gurnee <pegurnee@gmail.com>
2015-09-14 21:11:54 -0400

    checked if already exists in window

--- ajaxer.js
+++ ajaxer.js
@@ -2,7 +2,7 @@
 (function() {
   var
     prepareData = function(data) {
-      if (!data) {
+      if (typeof data !== "object") {
         return;
       }
       return Object.keys(data).map(function(k) {
@@ -10,6 +10,9 @@
       }).join("&");
     },
     setCallback = function(fn, request) {
+      if (typeof fn !== "function") {
+        return;
+      }
       request.onreadystatechange = (function() {
         if (request.readyState === 4 && request.status === 200) {
           fn(request.responseText);
@@ -17,33 +20,46 @@
       }).bind(request);
     };
 
-  post = function(url, callback, data) {
-    this.connect("POST", url, callback, data);
-  }
-  
-  get = function(url, callback, data) {
-    this.connect("GET", url, callback, data);
-  }
+  var ajax = {
+    post: function(url, data, callback) {
+      ajax.connect("POST", url, callback, data);
+    },
 
-  connect = function(style, url, callback, data) {
-    var request = new XMLHttpRequest(),
-      sendData = prepareData(data);
+    get: function(url, data, callback) {
+      ajax.connect("GET", url, callback, data);
+    },
 
-    if (style === "GET") {
-      var urlParams = "?" + sendData;
-      url += urlParams;
-      sendData = "";
+    connect: function(style, url, data, callback) {
+      var request = new XMLHttpRequest(),
+        sendData = prepareData(data);
+
+      if (style === "GET") {
+        var urlParams = "?" + sendData;
+        url += urlParams;
+        sendData = "";
+      }
+
+      setCallback(callback, request);
+
+      request.open(style, url, true);
+
+      if (style === "POST") {
+        request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        request.setRequestHeader("Content-length", sendData.length);
+        request.setRequestHeader("Connection", "close");
+      }
+      request.send(sendData);
     }
-
-    setCallback(callback, request);
-
-    request.open(style, url, true);
-
-    if (style === "POST") {
-      request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-      request.setRequestHeader("Content-length", sendData.length);
-      request.setRequestHeader("Connection", "close");
-    }
-    request.send(sendData);
   }
+
+  if (!window.post) {
+    window.post = ajax.post;
+  }
+  if (!window.get) {
+    window.get = ajax.get;
+  }
+  if (!window.connect) {
+    window.connect = ajax.connect;
+  }
+  window.ajax = ajax;
 })();
