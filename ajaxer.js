2e6529ce6974d0a403de42ac70d07fec79c65674
Eddie Gurnee <pegurnee@gmail.com>
2015-09-16 19:42:34 -0400

    add support for requirator

--- ajaxer.js
+++ ajaxer.js
@@ -1,6 +1,6 @@
 ;
 (function(root, factory) {
-  if (typeof define === 'function' && define.amd) {
+  if (typeof define === 'function') {
     define(factory);
   } else if (typeof exports === 'object') {
     module.exports = factory();
