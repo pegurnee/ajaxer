76d76b55dde8e3a6dfa34b2cd604ffe5117fb2cf
Eddie Gurnee <pegurnee@gmail.com>
2015-09-18 02:24:46 -0400

    add coverage

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,9 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+
     pkg: grunt.file.readJSON('package.json'),
+
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> as of <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -11,15 +13,63 @@ module.exports = function(grunt) {
         dest: '<%= pkg.directories["build"] + "/" + pkg.name %>.min.js'
       }
     },
+
+
     jshint: {
       src: ['<%= pkg.main %>']
+    },
+
+
+    coveralls: {
+      ajaxer_coverage: {
+        src: 'coverage/lcov.info'
+      }
+    },
+    env: {
+      coverage: {
+        APP_DIR_FOR_CODE_COVERAGE: '../tests/coverage/instrument/lib/'
+      }
+    },
+    instrument: {
+      files: 'lib/*.js',
+      options: {
+        lazy: true,
+        basePath: 'tests/coverage/instrument/'
+      }
+    },
+    storeCoverage: {
+      options: {
+        dir: 'tests/coverage/reports'
+      }
+    },
+    makeReport: {
+      src: 'tests/coverage/reports/**/*.json',
+      options: {
+        type: 'lcov',
+        dir: 'tests/coverage/reports',
+        print: 'detail'
+      }
+    },
+
+    mochaTest: {
+      options: {
+        reporter: 'spec'
+      },
+      src: ['tests/*.js']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-coveralls');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-istanbul');
+  grunt.loadNpmTasks('grunt-env');
 
   grunt.registerTask('default', ['jshint']);
   grunt.registerTask('build', ['uglify']);
 
+  grunt.registerTask('coverage', ['jshint', 'env:coverage',
+    'instrument', 'mochaTest', 'storeCoverage', 'makeReport'
+  ]);
 };
